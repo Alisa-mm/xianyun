612172801054d1be17e8ba941e43d7abb326c27d
lalala <iuyangdaxia@163.com>
2020-02-14 16:11:55 +0800

    21-单个条件过滤完成
    起飞时间 航空公司 起飞机场  机型 单个条件渲染完成

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -3,20 +3,18 @@
     <el-row type="flex" justify="space-between">
       <!-- 顶部过滤列表 -->
       <div class="flights-content">
-         <!-- 过滤条件 -->
-          <FlightsFilters />
+        <!-- 过滤条件 -->
+        <FlightsFilters :data="cacheFlightsData" @getAirData="getAirData" />
 
         <!-- 航班头部布局 -->
         <FlightsListHead />
 
         <!-- 航班信息列表 -->
-        <FlightsItem v-for="(item,index) in dataList" 
-        :key="index" :data="item"
-         />
+        <FlightsItem v-for="(item,index) in dataList" :key="index" :data="item" />
 
         <!-- 分页 -->
         <!-- current-page：当前页数
-            page-size：当前的条数 -->
+        page-size：当前的条数-->
         <el-pagination
           @size-change="handleSizeChange"
           @current-change="handleCurrentChange"
@@ -44,15 +42,24 @@ import FlightsFilters from "@/components/air/flightsFilters.vue";
 export default {
   data() {
     return {
-      flightsData: {}, //航班总数据
-    //   dataList: [], //航班列表数据，用于循环flightsItem组件
+      flightsData: {
+        flights: [], //航班总数据
+        info: {}, //航班信息
+        options: {} //筛选选项
+      },
+      // 过滤条件触发时候需要修改数组列表`flightsData.flights`，这样原来的列表就会被覆盖了，所以需要缓存一份列表
+      cacheFlightsData: {
+        // 缓存一份数据，只会修改一次
+        flights: [],
+        info: {},
+        options: {}
+      },
       //当前页数
-      pageIndex:1,
+      pageIndex: 1,
       //当前条数
-      pageSize:5,
+      pageSize: 5,
       //总条数
-      total:0
-
+      total: 0
     };
   },
   components: {
@@ -68,35 +75,45 @@ export default {
         method: "GET",
         params: this.$route.query
       }).then(res => {
-        // console.log(res);
+        console.log(res);
         // 总数据
         this.flightsData = res.data,
-        // this.dataList = this.flightsData.flights;
-        // 总条数
-        this.total =this.flightsData.total
+        // 缓存一份新的列表数据数据，这个列表不会被修改
+        // 而flightsData会被修改
+        this.cacheFlightsData = {...res.data}
+          // 总条数
+          this.total = this.flightsData.total;
       });
     },
     // 切换条数时触发的事件
-    handleSizeChange(index){
-        this.pageSize=index
+    handleSizeChange(index) {
+      this.pageSize = index;
     },
     // 切换页面时触发的事件
-    handleCurrentChange(index){
-        this.pageIndex=index
+    handleCurrentChange(index) {
+      this.pageIndex = index;
+    },
+    // 获取过滤组件的过滤后的数组（arr就是过滤后的数组）
+    getAirData(arr){
+      this.flightsData.flights=arr;
+      //总条数
+      this.total=arr.length
+
     }
   },
-  computed:{
+  computed: {
     // 当前页面要展示的数组（要切割）
-    dataList(){
-        // 判断flightsData有没有值
-        if(!this.flightsData.flights){
-            return [];
-        }
-        // 第一页展示0-5，第二页展示5-10
-        const arr = this.flightsData.flights.slice(
-            (this.pageIndex-1)*this.pageSize,
-            this.pageIndex*this.pageSize);
-            return arr;
+    dataList() {
+      // 判断flightsData有没有值
+      if (!this.flightsData.flights) {
+        return [];
+      }
+      // 第一页展示0-5，第二页展示5-10
+      const arr = this.flightsData.flights.slice(
+        (this.pageIndex - 1) * this.pageSize,
+        this.pageIndex * this.pageSize
+      );
+      return arr;
     }
   },
   mounted() {
