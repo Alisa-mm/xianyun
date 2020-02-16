fb727e04c92998e0d4b648ccfeb10f7f78357979
lalala <iuyangdaxia@163.com>
2020-02-16 23:52:56 +0800

    35-修改axiosError中处理401/403错误
    query参数应该写在括号里面
    redirect(status,path,query)

--- plugins/axiosErro.js
+++ plugins/axiosErro.js
@@ -16,9 +16,11 @@ export default (data)=>{
             // 跳转到登录页
             console.log( data);
             // 重定向到登录页
-            data.redirect(200,"/user/login"),{
+            data.redirect(200,"/user/login",{
+                // returnUrl:就是在发送某个请求失败的时候的url路径
+                // 然后失败跳转到登录页进行登录，登录成功之后返回到原来的url路径里再进行下一步操作(这里可以从登录页回到订单页)
                 returnUrl:data.route.fullPath
-            }
+            })
         }
     })
 
