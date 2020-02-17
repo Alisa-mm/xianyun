8216da7f567ad5899bedc980e638955c29e5fbfa
lalala <iuyangdaxia@163.com>
2020-02-17 22:05:07 +0800

    38-获取订单详情
    新建订单付款页面 页面一加载就获取订单详情

---
+++ pages/air/pay.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="container">
+    <div class="main">
+      <div class="pay-title">
+        支付总金额
+        <span class="pay-price">￥ 1000</span>
+      </div>
+      <div class="pay-main">
+        <h4>微信支付</h4>
+        <el-row type="flex" justify="space-between" align="middle" class="pay-qrcode">
+          <div class="qrcode">
+            <!-- 二维码 -->
+            <canvas id="qrcode-stage"></canvas>
+            <p>请使用微信扫一扫</p>
+            <p>扫描二维码支付</p>
+          </div>
+          <div class="pay-example">
+            <img src="http://157.122.54.189:9093/images/wx-sweep2.jpg" alt />
+          </div>
+        </el-row>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    // 获取订单详情 这个请求要放在定时器里面
+    // 因为组件加载的要比store加载的要快 在组件加载 mounted刷新页面时 store还没有把token值返回
+    setTimeout(() => {
+      this.$axios({
+        url: "airorders/" + this.$route.query.id,
+        headers: {
+          Authorization: `Bearer ` + this.$store.state.user.userInfo.token
+        }
+      }).then(res => {
+        console.log(res);
+        // console.log(this.$route.query);//query 是一个对象
+      });
+    }, 1000);//定时器时间写为0 也可以 不影响
+  }
+};
+</script>
+
+<style scoped lang="less">
+.container {
+  background: #f5f5f5;
+  padding: 30px 0;
+
+  .main {
+    width: 1000px;
+    margin: 0 auto;
+
+    .pay-title {
+      text-align: right;
+      span {
+        font-size: 28px;
+        color: orangered;
+      }
+    }
+
+    .pay-main {
+      background: #fff;
+      margin-top: 10px;
+      border-top: 5px orange solid;
+      padding: 30px;
+
+      h4 {
+        font-size: 28px;
+        font-weight: normal;
+        margin-bottom: 10px;
+      }
+
+      .pay-qrcode {
+        padding: 0 80px;
+      }
+
+      .qrcode {
+        border: 1px #ddd solid;
+        padding: 15px;
+        height: fit-content;
+
+        #qrcode-stage {
+          width: 200px;
+          height: 200px;
+          margin-bottom: 10px;
+        }
+
+        p {
+          line-height: 2;
+          text-align: center;
+        }
+      }
+    }
+  }
+}
+</style>
