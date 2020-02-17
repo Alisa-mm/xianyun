c7163d155e33bb6a4884290ddf88530287630708
lalala <iuyangdaxia@163.com>
2020-02-17 21:21:22 +0800

    36-搜索历史记录保持5条

--- store/air.js
+++ store/air.js
@@ -14,7 +14,14 @@ export const state=()=>{
 export const mutations={
     // 把传入的搜索表单的数据存进history中 
     setHistory(state,data){
-        state.history.unshift(data)
+        // 把新的历史记录放在第一位（unshift是在数组前面添加 push是在数组后面）
+        state.history.unshift(data);
+        // 让历史记录只显示5条
+        if(state.history.length>5){
+            //当搜索记录大于5的时候才让数组长度等于5(不然搜索记录不足5条的时候会报错)
+            state.history.length=5;
+        }
+
     },
     //修改orderDetail的数据
     setOrderDetail(state,data){
